9cbf2013e40b0109cabe61daabbdf01c787d05bc
Santiago <santymillan95@gmail.com>
2024-10-12 19:14:58 -0500

    errors fixed

--- server/events/center-events.js
+++ server/events/center-events.js
@@ -1,23 +1,23 @@
 const db = require("../db");
 
-const { changeScreen, randomKeys, verifyKeys, animationKeys } = require("../events-server/events-server")
+const { changeScreen, verifyKeys, animationKeys } = require("../events-server/events-server")
 const { reciveKeys, reciveAnimation } = require("../events-users/eventsExampleHandlers")
-const { keyspressed } = require("../events-arduino/arduino-events")
+const { keyspressed } = require("../events-arduino/arduino-events");
+
 
 const serverEvents = (socket, io) => {
-  socket.on("event-user", changeScreen(socket, db, io));
-  socket.on("event-user", randomKeys(socket, db, io));
-  socket.on("event-user", verifyKeys(socket, db, io));
-  socket.on("event-user", animationKeys(socket, db, io));
+  socket.on("changeScreen", () => changeScreen(socket, io));
+  socket.on("event-user", () => verifyKeys(socket, db, io));
+  socket.on("event-user", () => animationKeys(socket, db, io));
+}
+
+const userEvents = (socket, io) => {
+  socket.on("event-user", () => reciveKeys(socket, db, io));
+  socket.on("event-user", () => reciveAnimation(socket, db, io));
 };
 
-const userEvents = () =>{
-  socket.on("event-user", reciveKeys(socket, db, io));
-  socket.on("event-user", reciveAnimation(socket, db, io));
-};
-
-const arduinoEvents = () =>{
-  socket.on("event-user", keyspressed(socket, db, io));
+const arduinoEvents = (socket, io) => {
+  socket.on("event-user", () => keyspressed(socket, db, io));
 };
 
 module.exports = { 
